113d33a2e9710785b1b626f78eb07ef747e54dd8
wdoyle123 <will.doyle30@gmail.com>
2024-12-03 23:09:27 +0000

    feat: working menu selector

--- scripts/service-menu.js
+++ scripts/service-menu.js
@@ -1,29 +1,56 @@
-const payroll = document.getElementById('payroll');
-const taxation = document.getElementById('taxation');
-const bookkeeping = document.getElementById('bookkeeping');
-const vat = document.getElementById('vat');
-const selfAssessment = document.getElementById('self-assessment');
-const accounts = document.getElementById('accounts');
-const content = document.getElementById('job-content');
+const payroll = document.getElementById("payroll");
+const taxation = document.getElementById("taxation");
+const bookkeeping = document.getElementById("bookkeeping");
+const vat = document.getElementById("vat");
+const selfAssessment = document.getElementById("self-assessment");
+const accounts = document.getElementById("accounts");
+const content = document.getElementById("job-content");
+
+const payrollImg = document.getElementById("payroll-img");
+const taxationImg = document.getElementById("taxation-img");
+const bookkeepingImg = document.getElementById("bookkeeping-img");
+const vatImg = document.getElementById("vat-img");
+const selfAssessmentImg = document.getElementById("self-assessment-img");
+const accountsImg = document.getElementById("accounts-img");
+
+const jobNavItems = [
+  payroll,
+  taxation,
+  bookkeeping,
+  vat,
+  selfAssessment,
+  accounts,
+];
+
+const elementToImgMap = {
+  payroll: payrollImg,
+  taxation: taxationImg,
+  bookkeeping: bookkeepingImg,
+  vat: vatImg,
+  "self-assessment": selfAssessmentImg,
+  accounts: accountsImg,
+};
 
 const isActive = (element, htmlContent) => {
-  element.addEventListener('click', () => {
-    [payroll, taxation, bookkeeping, vat, selfAssessment, accounts].forEach(el => {
-      el.style.backgroundColor = '';
-      el.style.color = 'black';
-    });
+  element.addEventListener("click", () => {
+    jobNavItems.forEach((el) => el.classList.remove("active"));
+    Object.values(elementToImgMap).forEach((img) =>
+      img.classList.remove("active-img"),
+    );
 
-    element.style.backgroundColor = "#004225";
-    element.style.color = 'white';
+    element.classList.add("active");
+    const correspondingImg = elementToImgMap[element.id];
+    if (correspondingImg) {
+      correspondingImg.classList.add("active-img");
+    }
 
     content.innerHTML = htmlContent;
   });
 };
 
-isActive(payroll, '<p>Payroll</p>');
-isActive(taxation, '<p>Tax Returns</p>');
-isActive(bookkeeping, '<p>Bookkeeping</p>');
-isActive(vat, '<p>VAT Returns</p>');
-isActive(selfAssessment, '<p>Self Assessment</p>');
-isActive(accounts, '<p>Accounts</p>');
-
+isActive(payroll, "<p>Payroll</p>");
+isActive(taxation, "<p>Tax Returns</p>");
+isActive(bookkeeping, "<p>Bookkeeping</p>");
+isActive(vat, "<p>VAT Returns</p>");
+isActive(selfAssessment, "<p>Self Assessment</p>");
+isActive(accounts, "<p>Accounts</p>");
